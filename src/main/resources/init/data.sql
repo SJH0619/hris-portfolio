a159b1d202dfab6a10d6657a901a480c1e947d56
SJH0619 <idiotGuy0619@outlook.com>
2024-06-10 17:31:52 +0900

    feat: date 형식을 문자열로 바꾸면서 data.sql 수정

--- src/main/resources/init/data.sql
+++ src/main/resources/init/data.sql
@@ -1,5 +1,5 @@
 insert into employee(
     ueid, employee_id, password, name, gender, birth, insert_at, insert_by
 ) values (
-    '0000', '000000000000', '0000000', '관리자', 'M', now(), now(), '시스템'
+    '0000', '000000000000', '0000000', '관리자', 'M', '19960619', '20240610', '시스템'
 );
