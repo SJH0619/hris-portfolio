4ff468a4c6ba2aecd3970e1c8592ace359441e6f
SJH0619 <idiotGuy0619@outlook.com>
2024-06-10 16:43:30 +0900

    feat: data.sql 작성 및 yml 활성화

---
+++ src/main/resources/init/data.sql
@@ -0,0 +1,5 @@
+insert into employee(
+    ueid, employee_id, password, name, gender, birth, insert_at, insert_by
+) values (
+    '0000', '000000000000', '0000000', '관리자', 'M', now(), now(), '시스템'
+);
