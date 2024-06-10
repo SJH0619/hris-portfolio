1d21e16b1f02d0c36f64325eff2e58393358ea46
SJH0619 <idiotGuy0619@outlook.com>
2024-06-10 17:17:16 +0900

    feat: date 형식을 문자열 형식으로 변경

--- src/main/resources/init/schema.sql
+++ src/main/resources/init/schema.sql
@@ -12,7 +12,7 @@ create table employee(
     password char(80) not null comment '비밀번호',
     name char(37) not null comment '이름',
     gender char(1) not null comment '성별',
-    birth date not null comment '생년월일',
+    birth char(8) not null comment '생년월일',
     personal_phone_number char(11) comment '개인 전화번호',
     external_tell_number char(11) comment '사내 외부번호',
     internal_tell_number char(11) comment '사내 내선번호',
@@ -20,13 +20,13 @@ create table employee(
     company_email_address char(50) comment '사내 이메일 주소',
     department_code char(2) comment '부서',
     job_title_code char(2) comment '직급',
-    join_date date comment '입사일',
-    resign_date date comment '퇴사일',
+    join_date char(8) comment '입사일',
+    resign_date char(8) comment '퇴사일',
     employment_type char(1) comment '근무 유형',
     remaining_days int default 0 not null comment '남은 연차',
-    insert_at date not null comment '생성일',
+    insert_at char(14) not null comment '생성일',
     insert_by char(4) not null comment '생성자',
-    update_at date comment '수정일',
+    update_at char(14) comment '수정일',
     update_by char(4) comment '수정자'
 );
 
@@ -35,19 +35,19 @@ create table department(
     department_name char(15) not null comment '부서명',
     parent_department_code char(2) comment '상위 부서 코드',
     department_manager_ueid char(4) comment '부서장 고유 식별자',
-    foundation_date date not null comment '설립일',
-    insert_at date not null comment '생성일',
+    foundation_date char(8) not null comment '설립일',
+    insert_at char(14) not null comment '생성일',
     insert_by char(4) not null comment '생성자',
-    update_at date comment '수정일',
+    update_at char(14) comment '수정일',
     update_by char(4) comment '수정자'
 );
 
 create table job_title(
     job_title_code char(2) primary key comment '직급 코드',
     job_title_name char(15) not null comment '직급명',
-    insert_at date not null comment '생성일',
+    insert_at char(14) not null comment '생성일',
     insert_by char(4) not null comment '생성자',
-    update_at date comment '수정일',
+    update_at char(14) comment '수정일',
     update_by char(4) comment '수정자'
 );
 
@@ -58,24 +58,24 @@ create table salary(
     base_salary int comment '기본급',
     performance_bonus int comment '성과급',
     special_bonus int comment '특수급',
-    insert_at date not null comment '생성일',
+    insert_at char(14) not null comment '생성일',
     insert_by char(4) not null comment '생성자',
-    update_at date comment '수정일',
+    update_at char(14) comment '수정일',
     update_by char(4) comment '수정자'
 );
 
 create table performance_evaluation(
     ueid char(4) comment '직원 고유 식별자',
     seq int auto_increment comment '순번',
-    performance_evaluation_start_date date not null comment '성과 평가 시작 날짜',
-    performance_evaluation_end_date date comment '성과 평가 종료 날짜',
+    performance_evaluation_start_date char(8) not null comment '성과 평가 시작 날짜',
+    performance_evaluation_end_date char(8) comment '성과 평가 종료 날짜',
     performance_evaluation_item text comment '성과 평가 항목',
     performance_evaluation_result text comment '성과 평가 결과',
     performance_evaluation_comment text comment '성과 평가 비고',
     performance_evaluation_responsibility char(4) not null comment '성과 평가자',
-    insert_at date not null comment '생성일',
+    insert_at char(14) not null comment '생성일',
     insert_by char(4) not null comment '생성자',
-    update_at date comment '수정일',
+    update_at char(14) comment '수정일',
     update_by char(4) comment '수정자',
     constraint performance_evaluation_pk primary key(ueid, seq)
 );
@@ -84,31 +84,31 @@ create table vacation(
     ueid char(4) comment '직원 고유 식별자',
     seq int auto_increment comment '순번',
     vacation_type_code char(1) comment '휴가 종류',
-    start_date date comment '시작 날짜',
-    end_date date comment '종료 날짜',
+    start_date char(8) comment '시작 날짜',
+    end_date char(8) comment '종료 날짜',
     using_days int comment '사용 일수',
     remaining_days int comment '잔여 일수',
     vacation_purpose text comment '휴가 목적',
     approval_responsibility char(4) comment '승인자',
-    vacation_request_date date comment '휴가 신청 날짜',
-    vacation_approval_date date comment '휴가 승인 날짜',
-    insert_at date not null comment '생성일',
+    vacation_request_date char(8) comment '휴가 신청 날짜',
+    vacation_approval_date char(8) comment '휴가 승인 날짜',
+    insert_at char(14) not null comment '생성일',
     insert_by char(4) not null comment '생성자',
-    update_at date comment '수정일',
+    update_at char(14) comment '수정일',
     update_by char(4) comment '수정자',
     constraint vacation_pk primary key(ueid, seq)
 );
 
 create table attendance(
     ueid char(4) comment '직원 고유 식별자',
-    attendance_date date comment '날짜',
+    attendance_date char(8) comment '날짜',
     attendance_day char(1) not null comment '요일',
-    arrival_time date not null comment '출근 시각',
-    departure_time date not null comment '퇴근 시각',
+    arrival_time char(6) not null comment '출근 시각',
+    departure_time char(6) not null comment '퇴근 시각',
     remarks text comment '비고',
-    insert_at date not null comment '생성일',
+    insert_at char(14) not null comment '생성일',
     insert_by char(4) not null comment '생성자',
-    update_at date comment '수정일',
+    update_at char(14) comment '수정일',
     update_by char(4) comment '수정자',
     constraint attendance_pk primary key(ueid, attendance_date)
 );
